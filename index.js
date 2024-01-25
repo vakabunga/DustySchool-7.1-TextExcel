a000ba4526895116f058fd75b9876d91e49e9817
vakabunga <shesterov@gmail.com>
2024-01-25 20:32:07 +0300

    tried to sort object in another way, but it was useless

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const asTable = require('as-table');
-const helper = require('csvtojson');
+const csv = require('csvtojson');
 
 const csvFilePath = './sample1.csv';
-const csv = require('csvtojson')
 csv()
   .fromFile(csvFilePath)
   .then((result) => {
+    // a futile attempt to sort the object
     const tableHeader = Object.keys(result[0]);
     const tableHeaderNumbers = tableHeader.filter(item => typeof +item === 'number' && !isNaN(+item)).sort((a, b) => +a - +b);
     const tableHeaderStrings = tableHeader.filter(item => typeof item === 'string' && isNaN(+item)).sort((a, b) => b < a ? -1 : 1);
     const tableHeaderSorted = [...tableHeaderStrings, ...tableHeaderNumbers];
     const sortedJson = [];
 
-    // Sorting the Object by sorted Keys doesn't work
     for (const row of result) {
+      const map = new Map();
       const sortedRow = tableHeaderSorted.reduce((obj, header) => {
-        obj[header] = row[header];
+        obj[String(header)] = row[header];
         return obj;
-      }, {});
+      }, map);
       sortedJson.push(sortedRow);
     }
 
